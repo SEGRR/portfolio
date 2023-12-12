321e080034f9e6af1d46f4e51bbbfafe34af438a
shaikh sufiyan <61743021+SEGRR@users.noreply.github.com>
2023-12-12 15:52:22 +0000

    Design is complete

--- portfolio/src/components/Timer.jsx
+++ portfolio/src/components/Timer.jsx
@@ -4,62 +4,35 @@ import { useState, useEffect, useRef } from 'react';
 
 export default function Timer(){
 
-    const timing = {
-        day: (1000 * 60 * 60 * 24),
-        hour : (1000 * 60 * 60),
-        minute : (1000 * 60),
-        second : 1000
-    }
-
-    const day = (time)=>{
-        return Math.floor(time / timing.day );
-    }
-
-    const hour = (time)=>{
-        return Math.floor((time % timing.day) / timing.hour);
-    }
-
-    const minute = (time)=>{
-        return Math.floor(((time % timing.day) % timing.hour) / timing.minute);
-    }
-
-    const second = (time)=>{
-        return Math.floor((((time % timing.day)% timing.hour) % timing.minute) / timing.second)
-    }
-
+    const rollout_time = new Date("01/01/2024").getTime();
+    const default_value = rollout_time - new Date().getTime(); 
+    const [time , setTime] = useState(default_value);
     const timer = useRef();
 
-    const rollout_time =  Date.parse("01/01/2024");
-    const default_value = rollout_time - Date.now(); 
+    const second = 1000;
+    const minute = second * 60;
+    const hour =  minute * 60;
+    const day =   hour * 24;
 
-   const [time , setTime] = useState(default_value);
+   
 
-//    {
-//     day: day(default_value),
-//     hour: hour(default_value),
-//     minute: minute(default_value),
-//     second: second(default_value)
-//    }
-     
+  
     useEffect(()=>{
        timer.current =  setInterval(()=>{
-            setTime(prev => {
-                return prev - 1;
-            })
+            setTime(prev => prev-1)
         }, 1000);
 
-     //   return ()=> clearInterval(timer.current);
+        return ()=> clearInterval(timer.current);
       
-    });
+    }, []);
 
 
     return (
-        <div>
-            
-            <div><h1>{ day(time) }</h1></div>
-            <div><h1>{ hour(time) }</h1></div>
-            <div><h1>{ minute(time) }</h1></div>
-            <div><h1>{ second(time)  }</h1></div>
+        <div className='countdown-container'>
+            <div className='countdown-inner'><div className='time'><h1>{ Math.floor( time / day) }</h1></div><p>Day</p></div>
+            <div className='countdown-inner'><div className='time'><h1>{ Math.floor( (time % day) / hour) }</h1></div><p>Hour</p></div>
+            <div className='countdown-inner'><div className='time'><h1>{ Math.floor( (time % hour) / minute) }</h1></div><p>Minute</p></div>
+            <div className='countdown-inner'><div className='time'><h1>{ Math.floor( (time % minute) / second)  }</h1></div><p>Second</p></div>
         </div>
     )
 }
